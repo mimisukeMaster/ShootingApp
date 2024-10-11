e49f9021cf1d98406cc9a1e861f6291f29248b0f
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2024-10-12 02:35:39 +0900

    制作のひな形を用意

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ゲームの進行、スコア判定など総括的な処理を行うスクリプト
+public class GameManagerController : MonoBehaviour
+{
+    //スコア表示用テキスト
+    public TextMeshProUGUI text;
+
+    // Start関数は、ゲーム開始直後一回だけ実行される
+    void Start()
+    {
+        
+    }
+
+    // Update関数は毎フレームごとに実行される
+    void Update()
+    {
+        
+    }
+}
